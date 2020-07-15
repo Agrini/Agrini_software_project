6fb4c2c2312c6358ef50640d83cca89b3bcd0afc
Agrini Chaturvedi <17bec005@nirmauni.ac.in>
2020-07-15 19:58:48 +0530

    Final Word-Synonym Database File

--- Database/WordDB.sql
+++ Database/WordDB.sql
@@ -1,16 +1,24 @@
 use word_synonym;
 drop table word_synonyms;
+drop table word;
+drop table ws_table;
+drop table word_synonym_has_synonyms;
 create table word
  (	
-	word varchar(255) not null,
+	word_id integer ,
+    word varchar(255) not null,
 	primary key (word)
 );
 create table ws_table 
-(	word varchar(255) not null, 
-	synonym varchar(255) not null,
-    primary key (word, synonym)
+(	
+	sw_id integer  auto_increment ,
+    word varchar(255) not null, 
+	synonyms varchar(255) not null,
+    primary key(sw_id)
 );
 select * from ws_table;
 select * from word;
 drop table word;
 drop table ws_table; 
+select * from hibernate_sequence;
+create table word (word varchar(255) not null, word_id integer, primary key (word));
